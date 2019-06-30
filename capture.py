d23221c37d60d9d833de5ca8e9bf43aaec426cdc
Chak Koppula <chak.koppula199@gmail.com>
2019-06-30 21:35:54 +0530

    Program now tells the fps of the webcam after program has finsihed running

--- capture.py
+++ capture.py
@@ -1,18 +1,22 @@
 import cv2, time
 
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+#face_cascade=cv2.CascadeClassifier("haarcascade_eye.xml")
+#face_cascade=cv2.CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml")
+#face_cascade=cv2.CascadeClassifier("haarcascade_lefteye_2splits.xml")
+#face_cascade=cv2.CascadeClassifier("haarcascade_smile.xml")
 video=cv2.VideoCapture(0)
 
 fr = 1
-
+start = time.time()
 while True:
     fr+=1
     check, frame = video.read()
     faces=face_cascade.detectMultiScale(frame,
-    scaleFactor=1.1,
+    scaleFactor=1.05,
     minNeighbors=5)
     for x, y, width, height in faces:
-        frame=cv2.rectangle(frame, (x,y), (x+width, y+height), (0, 0, 255), 3)
+        frame=cv2.rectangle(frame, (x,y), (x+width, y+height), (0, 0, 255), 2)
 
     #gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Capturing", frame)
@@ -20,7 +24,11 @@ while True:
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
+end = time.time()
 
-print(fr)
+time_taken = end - start
+minutes = time_taken / 60
+frame_rate = fr/minutes/60
+print("Framerate was " + str(frame_rate) + " frames per second")
 video.release()
 cv2.destroyAllWindows()
