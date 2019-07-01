942fdfe1055a48c5bf1b8f115cd80d45c278606d
Chak Koppula <chak.koppula199@gmail.com>
2019-07-01 15:50:36 +0530

    Added blurred garyscale frame, difference frame (delta) and a threshold frame to the program

--- MotionDetector.py
+++ MotionDetector.py
@@ -1,28 +1,45 @@
 import cv2, time
 
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# This is used to store the first image seen by the camaera and
+# acts as a reference for following images to be compared against
+reference_frame = None
+
+# # starts the camera, argument should  be changed if multiple cameras are available
 video=cv2.VideoCapture(0)
 
 while True:
-    start=time.time()
+
+    # captures boolean and numpy array from camera
     check, frame = video.read()
-    faces=face_cascade.detectMultiScale(frame,
-    scaleFactor=1.05,
-    minNeighbors=5)
-    for x, y, width, height in faces:
-        frame=cv2.rectangle(frame, (x,y), (x+width, y+height), (0, 0, 255), 2)
 
+    # converts image to a gray version for more accuracy later on
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Capturing", gray)
+    gray=cv2.GaussianBlur(gray, (21, 21,), 0) # smooths edges and reduces noise in calculations
 
+    #checks to see if we need to assign the reference frame a value
+    if reference_frame is None:
+        reference_frame = gray
+        continue
+
+    # calculates diffeence between reference_frame and current fram and stores as an image
+    delta_frame=cv2.absdiff(reference_frame, gray)
+
+    # makes any pixel with a difference larger than athreshold white, else black
+    thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame, None,iterations=2)
+
+    # finds conours of distinct objects in the frame
+    (_,cnts,_)=cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # displays images
+    cv2.imshow("Threshold", thresh_frame)
+    cv2.imshow("Delta", delta_frame)
+    cv2.imshow("Gray Frame", gray)
+
+    # if q is pressed then loop is exited
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
-    end = time.time()
 
-time_taken = end - start
-minutes = time_taken / 60
-frame_rate = fr/minutes/60
-print("Framerate was " + str(frame_rate) + " frames per second")
 video.release()
 cv2.destroyAllWindows()
