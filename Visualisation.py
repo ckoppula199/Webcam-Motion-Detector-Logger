39eed219c86807287d0516852b3473da246f49ce
Chak Koppula <chak.koppula199@gmail.com>
2019-07-03 20:55:15 +0530

    Completed hover feature

--- Visualisation.py
+++ Visualisation.py
@@ -2,13 +2,16 @@ from bokeh.plotting import show, figure, output_file
 from bokeh.models import HoverTool, ColumnDataSource
 from MotionDetector import df
 
+df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+cds = ColumnDataSource(df)
+
 p = figure(x_axis_type = 'datetime', height=100, width=500, sizing_mode='scale_width', title='Motion Graph')
 p.yaxis.minor_tick_line_color = None
 p.ygrid[0].ticker.desired_num_ticks=1
 
-cds = ColumnDataSource(df)
-
-hover = HoverTool(tooltips=[("Start", "@Start"), ("End", "@End")])
+hover = HoverTool(tooltips=[("Start", "@Start_string"), ("End", "@End_string")])
 p.add_tools(hover)
 
 
